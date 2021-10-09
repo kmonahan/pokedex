b39fd978ee96ab799e691709b326c4cb65bc2982
KJ Monahan <kj@forumone.com>
2021-10-09 17:04:31 -0500

    Preserve theme selection between page loads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "../source/base/global.css";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { theme } from "../source/base/theme.css";
 import { mainCss } from "../source/components/Layout/main.css";
 import ThemeSelector from "../source/components/ThemeSelector";
@@ -10,7 +10,22 @@ type Theme = "sun" | "moon";
 
 function Pokedex({ Component, pageProps }: AppProps) {
   const [themeClass, setThemeClass] = useState<Theme>("moon");
-  const changeTheme = (newTheme: Theme) => setThemeClass(newTheme);
+  const changeTheme = (newTheme: Theme) => {
+    setThemeClass(newTheme);
+    if (window.localStorage) {
+      window.localStorage.setItem("selectedTheme", newTheme);
+    }
+  };
+
+  useLayoutEffect(() => {
+    if (window.localStorage) {
+      const selectedTheme = window.localStorage.getItem("selectedTheme");
+      if (selectedTheme) {
+        setThemeClass(selectedTheme as Theme);
+      }
+    }
+  }, [setThemeClass]);
+
   return (
     <div className={`${themeClass} ${theme} ${mainCss}`}>
       <Link href="/">Home</Link>
